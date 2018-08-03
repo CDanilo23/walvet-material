e69b292fc500784375b8ebc427fe85f4d0ff6e45
Cristian Danilo Ordoñez <cristian.ordonez@globant.com>
2018-08-03 16:53:11 -0500

    disable fields

--- src/app/clinicstorydetails/clinicstorydetails.component.ts
+++ src/app/clinicstorydetails/clinicstorydetails.component.ts
@@ -1,32 +1,66 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { ClinicstoryService } from '../service/clinicstory.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Clinicstory } from '../clinicstory/clinicstory';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {FormControl} from '@angular/forms';
+import { MY_FORMATS } from '../constants/DateFormats';
+import * as _moment from 'moment';
+import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+
 
 
 @Component({
   selector: 'app-clinicstorydetails',
   templateUrl: './clinicstorydetails.component.html',
-  styleUrls: ['./clinicstorydetails.component.scss']
+  styleUrls: ['./clinicstorydetails.component.scss'],
+  providers: [
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
+  ],
 })
 export class ClinicstorydetailsComponent implements OnInit, OnDestroy {
 
-  clinicstory: Clinicstory;
+  date = new FormControl(_moment());
+  born_date = new FormControl(_moment());
+  story: Clinicstory;
   private sub: any;
   id: Number;
+  MY_FORMATS;
+  enableAllInputs: boolean;
+
+
+
   constructor(private clinicstoryservice: ClinicstoryService,
               private activatedroute: ActivatedRoute,
-              private route: Router) { }
+              private route: Router) {
+                this.enableAllInputs = true;
+               }
 
   ngOnInit() {
+
     this.sub = this.activatedroute.params.subscribe(params => {
       this.id = params['id'];
-      this.clinicstoryservice.findById(this.id).subscribe(p => this.clinicstory = p);
+      this.clinicstoryservice.findById(this.id).subscribe(p => this.story = p);
     });
+  }
+
+
+  enableInputs() {
+    this.enableAllInputs = false;
+    console.log('----------entro---------');
   }
 
   ngOnDestroy() {
     this.sub.unsubscribe();
+  }
+
+  editValues(): void {
+    this.story.fecha = this.date.value;
+    console.log('converted ' +  this.story.fecha);
+    this.story.born_date = this.born_date.value;
+    console.log('converted ' + this.story.born_date);
   }
 
 }
